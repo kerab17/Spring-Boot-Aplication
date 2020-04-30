c3cc26506065ef95b11a0e3cf102d6162754aef8
kevin <kevin@192.168.1.87>
2020-04-30 15:22:37 -0500

    Comit Inicial con Spring tools. Close #1

---
+++ SpringAplicacion/src/main/java/com/kerab/SpringAplicacion/SpringAplicacionApplication.java
@@ -0,0 +1,13 @@
+package com.kerab.SpringAplicacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAplicacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAplicacionApplication.class, args);
+	}
+
+}
---
+++ SpringAplicacion/src/test/java/com/kerab/SpringAplicacion/SpringAplicacionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kerab.SpringAplicacion;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringAplicacionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
